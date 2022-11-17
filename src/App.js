dc3c44286a9c67d5c087180976bd7068e3b805d7
Jason Comisi <jaycomisi918@gmail.com>
2022-11-17 16:53:06 -0500

    added initializing code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Card Game</h1>
+      <button>Start Game</button>
+    </div>
+  );
+}
+
+export default App;
